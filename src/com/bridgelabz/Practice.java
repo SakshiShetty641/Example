7e0732834e5955f5929009c4f913f6a8ca27e8c9
Sakshi <sakshishetty1231997@gmail.com>
2021-08-04 20:39:40 +0530

    New File added

--- src/com/bridgelabz/Practice.java
+++ src/com/bridgelabz/Practice.java
@@ -3,6 +3,10 @@ package com.bridgelabz;
 public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the program");
+		int a = 10;
+		int b = 10;
+		int add = a + b;
+		System.out.println(add);
 	}
 
 }
